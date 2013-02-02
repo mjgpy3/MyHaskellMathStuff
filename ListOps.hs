2dc6aac404ae3261fe2e2e968f011d1d83e01ddf
Michael Gilliland <mjg.py3@gmail.com>
2013-02-02 13:20:06 -0500

    Added my max and min functions in

--- ListOps.hs
+++ ListOps.hs
@@ -1,6 +1,20 @@
 module ListOps where
 
 mean :: (Fractional a) => [a] -> Maybe a
+max' :: (Ord a) => [a] -> a
+min' :: (Ord a) => [a] -> a
+
+-- If an empty is given return Nothing otherwise return the mean
 mean [] = Nothing
 mean xs = Just (sum xs) / (fromIntegral (length xs))
+
+max' (x:[]) = x
+max' (x:y:xs)
+ | x > y     = max' (x:xs)
+ | otherwise = max' (y:xs)
+
+min' (x:[]) = x
+min' (x:y:xs)
+ | x < y     = min' (x:xs)
+ | otherwise = min' (y:xs)
 

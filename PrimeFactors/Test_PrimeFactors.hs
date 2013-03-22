d0e9cdcd830ba0cd40d9170d2bef63ec169905e7
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-22 13:17:52 -0400

    Made PrimeFactors code hlint compliant

--- PrimeFactors/Test_PrimeFactors.hs
+++ PrimeFactors/Test_PrimeFactors.hs
@@ -3,62 +3,62 @@ module Test_PrimeFactor where
 import Test.HUnit
 import PrimeFactors
 
-five_is_prime_test =
+testFiveIsPrime =
  TestCase (assertEqual "5 should be prime" True (isPrime 5))
 
-two_is_prime_test =
+testTwoIsPrime =
  TestCase (assertEqual "2 should be prime" True (isPrime 2))
 
-three_is_prime_test =
+testThreeIsPrime =
  TestCase (assertEqual "3 should be prime" True (isPrime 3))
 
-four_is_not_prime_test =
+testFourIsNotPrime =
  TestCase (assertEqual "4 should not be prime" False (isPrime 4))
 
-fifteen_is_not_prime_test =
+testFifteenIsNotPrime =
  TestCase (assertEqual "15 should not be prime" False (isPrime 15))
 
-six_is_not_prime_test =
+testSixIsNotPrime =
  TestCase (assertEqual "6 should not be prime" False (isPrime 6))
 
-twenty_nine_is_prime_test =
+testTwentyNineIsNotPrime =
  TestCase (assertEqual "29 should be prime" True (isPrime 29))
 
-two_is_the_only_prime_factor_of_two_test =
+
+testTwoIsTheOnlyPrimeFactorOfTwo = 
  TestCase (assertEqual "2 -> [2]" [2] (getPrimeFactors 2))
 
-
-three_is_the_only_prime_factor_of_three_test =
+testThreeIsTheOnlyPrimeFactorOfThree =
  TestCase (assertEqual "3 -> [3]" [3] (getPrimeFactors 3))
 
-two_and_two_are_the_prime_factors_of_four_test =
+testTwoAndTwoAreThePrimeFactorsOfFour =
  TestCase (assertEqual "4 -> [2,2]" [2,2] (getPrimeFactors 4))
 
-two_and_three_are_the_prime_factors_of_six_test = 
+testTwoAndThreeAreThePrimeFactorsOfSix =
  TestCase (assertEqual "6 -> [2,3]" [2,3] (getPrimeFactors 6))
 
-three_and_three_are_the_prime_factors_of_nine_test =
+testThreeAndThreeAreThePrimeFactorsOfNine =
  TestCase (assertEqual "9 -> [3,3]" [3,3] (getPrimeFactors 9))
 
-two_two_and_three_are_the_prime_factors_of_twelve =
+testTwoTwoAndThreeAreThePrimeFactorsOfTwelve =
  TestCase (assertEqual "12 -> [2,2,3]" [2,2,3] (getPrimeFactors 12))
 
-three_and_five_are_the_prime_factors_of_fifteen =
+testThreeAndFiveAreThePrimeFactorsOfFifteen =
  TestCase (assertEqual "15 -> [3,5]" [3,5] (getPrimeFactors 15))
 
-tests = TestList [five_is_prime_test,
-                  two_is_prime_test,
-                  three_is_prime_test,
-                  four_is_not_prime_test,
-                  fifteen_is_not_prime_test,
-                  six_is_not_prime_test,
-                  twenty_nine_is_prime_test,
-                  two_is_the_only_prime_factor_of_two_test,
-                  three_is_the_only_prime_factor_of_three_test,
-                  two_and_two_are_the_prime_factors_of_four_test,
-                  two_and_three_are_the_prime_factors_of_six_test,
-                  three_and_three_are_the_prime_factors_of_nine_test,
-                  two_two_and_three_are_the_prime_factors_of_twelve,
-                  three_and_five_are_the_prime_factors_of_fifteen]
+tests = TestList [testFiveIsPrime,
+                  testTwoIsPrime,
+                  testThreeIsPrime,
+                  testFourIsNotPrime,
+                  testFifteenIsNotPrime,
+                  testSixIsNotPrime,
+                  testTwentyNineIsNotPrime,
+                  testTwoIsTheOnlyPrimeFactorOfTwo, 
+                  testThreeIsTheOnlyPrimeFactorOfThree,
+                  testTwoAndTwoAreThePrimeFactorsOfFour,
+                  testTwoAndThreeAreThePrimeFactorsOfSix,
+                  testThreeAndThreeAreThePrimeFactorsOfNine,
+                  testTwoTwoAndThreeAreThePrimeFactorsOfTwelve,
+                  testThreeAndFiveAreThePrimeFactorsOfFifteen]
 
-main = do runTestTT tests
+main = runTestTT tests

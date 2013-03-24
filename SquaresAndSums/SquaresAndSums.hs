1dc98290ce9cc762e4432d5fddb3e13fe328ecd7
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-24 17:53:22 -0400

    Made SOS hlint compliant

--- SquaresAndSums/SquaresAndSums.hs
+++ SquaresAndSums/SquaresAndSums.hs
@@ -4,4 +4,4 @@ squareOfSums :: Integer -> Integer
 sumOfSquares :: Integer -> Integer
 
 squareOfSums n = ((n*(n+1)) `quot` 2)^2
-sumOfSquares n = ((n*(n+1)*(2*n+1)) `quot` 6)
+sumOfSquares n = (n*(n+1)*(2*n+1)) `quot` 6

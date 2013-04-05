3be2ddb91f70be1ad6724c67dfb90d16c39de9a8
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-05 12:19:09 -0400

    Figured out how to export data and methods in modules

--- DataLearning/DataLearning.hs
+++ DataLearning/DataLearning.hs
@@ -1,13 +1,32 @@
-module DataLearning where
+module DataLearning (VideoGame(VideoGame),
+                     title, rating, year,
+                     bestGame,
+                     sortByRating) where
 
+import Data.Maybe
+
+-- Public types
 bestGame :: [VideoGame] -> Maybe VideoGame
+sortByRating :: [VideoGame] -> [VideoGame]
 
 data VideoGame = VideoGame { title :: String,
                              rating :: Float,
-                             year :: Int } deriving (Show)
+                             year :: Int } deriving (Show, Eq)
+
+-- Private
+--removeFromList
 
 bestGame [] = Nothing
 bestGame [g@(VideoGame _ _ _)] = Just g
 bestGame (g1@(VideoGame _ r1 _):g2@(VideoGame _ r2 _):gs)
  | r1 > r2   = bestGame (g1:gs)
  | otherwise = bestGame (g2:gs)
+
+sortByRating [] = []
+sortByRating [g] = [g]
+sortByRating  gs = [fromJust (bestGame gs)]
+
+a = VideoGame "test" 0.0 1
+b = VideoGame "" 0.0 1
+
+main = print (a == b)
--- DataLearning/Test_DataLearning.hs
+++ DataLearning/Test_DataLearning.hs
@@ -2,16 +2,21 @@ module Test_DataLearning where
 
 import Test.HUnit
 import DataLearning
+import Data.Maybe
 
-vlg = [VideoGame "Goldeneye 007" 9.6 1997,
+vgl = [VideoGame "Goldeneye 007" 9.6 1997,
        VideoGame "Bioshock Infinite" 9.8 2013,
        VideoGame "Sonic the Hedgehog 2" 9.0 1992,
        VideoGame "Star Wars: Knights of the Old Republic" 9.0 2003,
        VideoGame "Doom 3" 8.9 2004]
 
 testBioshockInfiniteIsRankedBestGame =
-  TestCase (assertEqual "Bioshock is listed as the best game" "Bioshock Infinite" (maybe "" title (bestGame vlg)))
+  TestCase (assertEqual "Bioshock is listed as the best game" "Bioshock Infinite" ( title (fromJust (bestGame vgl))))
 
-tests = TestList [testBioshockInfiniteIsRankedBestGame]
+testGamesAreEquatable =
+  TestCase (assertEqual "A = A" True (vgl !! 0 == vgl !! 0))
+
+tests = TestList [testBioshockInfiniteIsRankedBestGame,
+                  testGamesAreEquatable]
 
 main = runTestTT tests

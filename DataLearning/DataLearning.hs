cf37157d9837b9cd7bb67a3f266fe2955823e313
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-06 10:47:34 -0400

    Added many tests and began debugging the sortByRating function

--- DataLearning/DataLearning.hs
+++ DataLearning/DataLearning.hs
@@ -9,25 +9,30 @@ import Data.Maybe
 -- Public types
 bestGame :: [VideoGame] -> Maybe VideoGame
 sortByRating :: [VideoGame] -> [VideoGame]
+removeFromList :: (Eq a) => a -> [a] -> [a]
 
 data VideoGame = VideoGame { title :: String,
                              rating :: Float,
                              year :: Int } deriving (Show, Eq)
 
 -- Private
-removeFromList :: (Eq a) => a -> [a] -> [a]
+betterGame :: VideoGame -> VideoGame -> VideoGame
+
+betterGame g1@(VideoGame _ r1 _) g2@(VideoGame _ r2 _)
+ | r1 > r2  = g1
+ | otherwise = g2
 
 bestGame [] = Nothing
-bestGame [g@(VideoGame _ _ _)] = Just g
-bestGame (g1@(VideoGame _ r1 _):g2@(VideoGame _ r2 _):gs)
- | r1 > r2   = bestGame (g1:gs)
- | otherwise = bestGame (g2:gs)
+bestGame [g] = Just g
+bestGame (g1:g2:gs) = bestGame ((betterGame g1 g2):gs)
 
 sortByRating [] = []
 sortByRating [g] = [g]
-sortByRating  gs = [fromJust (bestGame gs)]
+sortByRating  gs = currentTop:(removeFromList currentTop gs)
+ where currentTop = fromJust (bestGame gs)
 
 removeFromList _ [] = []
+removeFromList e [x] = if e == x then [] else [x]
 removeFromList e (x:xs)
  | e == x    = xs
  | otherwise = x:removeFromList e xs
--- DataLearning/Test_DataLearning.hs
+++ DataLearning/Test_DataLearning.hs
@@ -7,13 +7,19 @@ import Data.List
 
 extra_game = VideoGame "Fallout 3" 8.8 2008
 
-vgl = [VideoGame "Goldeneye 007" 9.6 1997,
-       VideoGame "Bioshock Infinite" 9.8 2013,
-       VideoGame "Sonic the Hedgehog 2" 9.0 1992,
-       VideoGame "Star Wars: Knights of the Old Republic" 9.0 2003,
-       VideoGame "Doom 3" 8.9 2004,
-       VideoGame "Morrowind" 9.6 2003]
+g1 = VideoGame "Goldeneye 007" 9.6 1997
+g2 = VideoGame "Bioshock Infinite" 9.8 2013
+g3 = VideoGame "Sonic the Hedgehog 2" 9.0 1992
+g4 = VideoGame "Star Wars: Knights of the Old Republic" 9.0 2003
+g5 = VideoGame "Doom 3" 8.9 2004
+g6 = VideoGame "Morrowind" 9.6 2003
 
+vgl = [g1, g2, g6, g3, g4, g5]
+vgl_sorted_by_rating = [g2, g1, g6, g3, g4, g5]
+
+vgl_redundant_bests = [g1, g3, g4, g5, g6]
+
+vgl_best_at_end = [g1, g3, g4, g5, g6, g2]
 vgl2 = extra_game:vgl
 
 list_of_ints = [1,2..20]
@@ -24,6 +30,9 @@ expected_randoms = [3, 5]
 
 testBioshockInfiniteIsRankedBestGame =
   TestCase (assertEqual "Bioshock is listed as the best game" "Bioshock Infinite" ( title (fromJust (bestGame vgl))))
+
+testBestGameWorksIfAtTheEnd =
+  TestCase (assertEqual "The best game should be the game at the end" g2 (fromJust (bestGame vgl_best_at_end)))
 
 testGamesAreEquatable =
   TestCase (assertEqual "A = A" True (vgl !! 0 == vgl !! 0))
@@ -40,15 +49,28 @@ testIfToldToRemoveSomethingNotInTheListThenReturnTheList =
 testRemovingSomethingFromTheEmptyListReturnsTheEmptyList =
   TestCase (assertEqual "Removing something from [] yields []" [] (removeFromList 2354 []))
 
+testRemovingSomethingFromTheHighestIndexOfAListWorks =
+  TestCase (assertEqual "Removing 20 from [1..20] yields [1..19]" [1, 2..19] (removeFromList 20 [1,2..20]))
+
 testCanDeleteAVideoGameFromAListOfVideoGames =
   TestCase (assertEqual "Should be able to successfully delete a video game" vgl (removeFromList extra_game vgl2))
 
+testSortByRatingWorksOnAListOfGames =
+  TestCase (assertEqual "sortByRating should work on list of games" vgl_sorted_by_rating (sortByRating vgl))
+
+testIfRedundantBestsSecondIsFound =
+  TestCase (assertEqual "When we have redundant best games, the second is the one we care about" g6 (fromJust (bestGame vgl_redundant_bests)))
+
 tests = TestList [testBioshockInfiniteIsRankedBestGame,
+                  testBestGameWorksIfAtTheEnd,
                   testGamesAreEquatable,
                   testRemoveFromListRemovesFromListSuccessfully,
                   testRemoveFromListWorksForANonSetLikeCollection,
                   testIfToldToRemoveSomethingNotInTheListThenReturnTheList,
                   testRemovingSomethingFromTheEmptyListReturnsTheEmptyList,
-                  testCanDeleteAVideoGameFromAListOfVideoGames]
+                  testRemovingSomethingFromTheHighestIndexOfAListWorks,
+                  testCanDeleteAVideoGameFromAListOfVideoGames,
+                  testSortByRatingWorksOnAListOfGames,
+                  testIfRedundantBestsSecondIsFound]
 
 main = runTestTT tests

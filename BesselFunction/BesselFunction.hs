35334245daaafd7c637688bc2ec00d87d2ab56e8
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-10 01:49:29 -0400

    Tested and coded a Bessel Function to some extent... Some bugs to work out

--- BesselFunction/BesselFunction.hs
+++ BesselFunction/BesselFunction.hs
@@ -1,7 +1,17 @@
-module BesselFunction (factorial) where
+module BesselFunction (factorial,
+                       j0) where
 
---j0 :: (Num a) => a -> Int -> a
+j0 :: (Num a, Fractional a) => a -> Int -> a
 factorial :: Int -> Int
+f :: (Num a, Fractional a) => a -> Int -> a
+
+f x k = (((-1)^k)/( fromIntegral (factorial k)^2))*((x/2.0)^(2*k))
+
+j0 x n
+ | n > 33 = error "Cannot generate more than 33 terms"
+ | otherwise = 1 + sum [f x k | k <- [1..n]]
 
 factorial 1 = 1
-factorial n = n * (factorial (n-1))
+factorial n = n * factorial (n-1)
+
+main = print (sum [f 0.0 i | i <- [1..33]])
--- BesselFunction/Test_BesselFunction.hs
+++ BesselFunction/Test_BesselFunction.hs
@@ -3,6 +3,11 @@ module Test_BesselFunction where
 import Test.HUnit
 import BesselFunction
 
+epsilon = 0.005
+
+-- Find whether something fits in epsilon, v is value, e is expected
+withinEpsilon e v = e+epsilon >= v && e-epsilon <= v
+
 testFactorialOf3Is6 =
   TestCase (assertEqual "3! = 6" 6 (factorial 3))
 
@@ -12,8 +17,20 @@ testFactorialOf1Is1 =
 testFactorialOf5Is120 =
   TestCase (assertEqual "5! = 120" 120 (factorial 5))
 
+testJ0Of0Is1 =
+  TestCase (assertEqual "J_0(0) = 1" 1 (j0 0.0 33))
+
+testJ0OfAQuarterIsPointNineEight =
+  TestCase (assertEqual "J_0(0.25) = 0.98" True (withinEpsilon 0.98 (j0 0.25 33)))
+
+testJ0Of1IsNegativePointSeventySeven =
+  TestCase (assertEqual "J_0(1) = 0.77" True (withinEpsilon 0.77 (j0 1.0 33)))
+
 tests = TestList [testFactorialOf3Is6,
                   testFactorialOf1Is1,
-                  testFactorialOf5Is120]
+                  testFactorialOf5Is120,
+                  testJ0Of0Is1,
+                  testJ0OfAQuarterIsPointNineEight,
+                  testJ0Of1IsNegativePointSeventySeven]
 
 main = runTestTT tests

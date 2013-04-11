c92d28d029f32752ba5731fa4476f051efc7b059
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-11 00:43:25 -0400

    Did some refactoring of sin

--- TrigFunctions/Trig.hs
+++ TrigFunctions/Trig.hs
@@ -1,9 +1,12 @@
 module Trig (sin') where
 
 sin' :: (Num a, Fractional a) => a -> a
+sinTerm :: (Num a, Fractional a) => a -> Integer -> a
 factorial :: Integer -> Integer
 
-sin' x = sum [(x^(2*i - 1) / (fromIntegral (factorial (2*i - 1))))*(-1.0)^(i-1) | i <- [1..33]]
+sin' x = sum [sinTerm x i | i <- [1..33]]
+sinTerm x i = (x^oddTerm / fromIntegral (factorial oddTerm))*(-1)^(i-1)
+  where oddTerm = 2*i - 1
 
 factorial 1 = 1
 factorial n = n * factorial (n-1)

ddafbdadf8eabe1b6ddf6f014f0dcafccad83bbe
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-22 19:08:21 -0400

    Made a row and column removal function

--- Matrix/MatrixOps.hs
+++ Matrix/MatrixOps.hs
@@ -4,3 +4,17 @@ determinant :: (Num a, Fractional a) => [[a]] -> a
 
 determinant [[x]] = x
 determinant [[w,x], [y,z]] = w*z - x*y
+determinant mat = sum [s*x*(determinant (getRest i mat)) | i <- [0..(n-1)],
+                                                           let x = (head mat) !! i]
+ where n = length $ head mat
+       s = (-1)^n
+
+getRest :: (Num a, Fractional a) => Int -> [[a]] -> [[a]]
+
+getRest i mat = removeCols i (tail mat)
+
+removeCols :: (Num a, Fractional a) => Int -> [[a]] -> [[a]]
+
+removeCols _ [] = []
+removeCols i (row:rows) = [x | j <- [0..(n-1)], j /= i, let x = row !! j] : (removeCols i rows)
+ where n = length row
--- Matrix/Test_MatrixOps.hs
+++ Matrix/Test_MatrixOps.hs
@@ -12,8 +12,19 @@ testDeterminantWorksForASimpleTwoByTwo =
 testDeterminantworksForAnotherSimpleTwoByTwo =
   TestCase (assertEqual "Determant of another 2x2 works" (-2) (determinant [[1,7], [0, -2]]))
 
+testGetRestWorksForAThreeByThreeForTheFirstSlot =
+  TestCase (assertEqual "First slot can be removed by getRest" [[5, 6], [8,9]] (getRest 0 [[1,2,3],[4,5,6],[7,8,9]]))
+
+testGetRestWorksForAThreeByThreeForTheSecondSlot =
+  TestCase (assertEqual "Second slot can be removed by getRest" [[4,6], [7,9]] (getRest 1 [[1,2,3],[4,5,6],[7,8,9]]))
+
+testGetRestWorksForAThreeByThreeForTheThirdSlot =
+  TestCase (assertEqual "Third slot can be removed by getRest" [[4,5], [7,8]] (getRest 2 [[1,2,3],[4,5,6],[7,8,9]]))
+
 tests = TestList [testDeterminantWorksForASimpleOneByOne,
                   testDeterminantWorksForASimpleTwoByTwo,
-                  testDeterminantworksForAnotherSimpleTwoByTwo]
+                  testDeterminantworksForAnotherSimpleTwoByTwo,
+                  testGetRestWorksForAThreeByThreeForTheFirstSlot,
+                  testGetRestWorksForAThreeByThreeForTheSecondSlot]
 
 main = runTestTT tests

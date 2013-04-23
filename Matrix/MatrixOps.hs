6c6037ae54cec2206124c474938d382841dfe80c
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-22 22:20:54 -0400

    Made code more HLint compliant... More because HLint is lying some...

--- Matrix/MatrixOps.hs
+++ Matrix/MatrixOps.hs
@@ -4,9 +4,9 @@ determinant :: (Num a, Fractional a) => [[a]] -> a
 
 determinant [[x]] = x
 determinant [[w,x], [y,z]] = w*z - x*y
-determinant mat = sum [s*x*(determinant (getRest i mat)) | i <- [0..(n-1)],
-                                                           let x = (head mat) !! i
-                                                               s = (-1)^i]
+determinant mat =
+ sum [s*x*(determinant (getRest i mat)) | i <- [0..n-1], let x = (head mat) !! i
+                                                             s = (-1)^i]
  where n = length $ head mat
 
 getRest :: (Num a, Fractional a) => Int -> [[a]] -> [[a]]
@@ -16,5 +16,5 @@ getRest i mat = removeCols i (tail mat)
 removeCols :: (Num a, Fractional a) => Int -> [[a]] -> [[a]]
 
 removeCols _ [] = []
-removeCols i (row:rows) = [x | j <- [0..(n-1)], j /= i, let x = row !! j] : (removeCols i rows)
- where n = length row
+removeCols i (r:rs) = [r !! j | j <- [0..n-1], j /= i] : removeCols i rs
+ where n = length r

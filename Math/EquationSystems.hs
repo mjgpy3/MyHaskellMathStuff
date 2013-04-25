a8ce61c4af0fcbcdd3bbb1b3112472024954cb98
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-25 18:48:41 -0400

    Moved lines around (I think this improved reading)

--- Math/EquationSystems.hs
+++ Math/EquationSystems.hs
@@ -2,15 +2,13 @@ module EquationSystems where
 
 import MatrixOps
 
-solveEquations :: (Num a, Fractional a) => [[a]] -> [a] -> [a]
-
 -- Solves equations of the form AX = B
+solveEquations :: (Num a, Fractional a) => [[a]] -> [a] -> [a]
 solveEquations a b = [ determinant (replaceCols i a b) / det | i <- [0..(length a)-1] ]
  where det = determinant a
 
 
 replaceCols :: (Num a, Fractional a) => Int -> [[a]] -> [a] -> [[a]]
-
 replaceCols _ [] [] = []
 replaceCols i (a:as) (b:bs) = (left ++ b:(tail right)) : replaceCols i as bs
  where (left, right) = splitAt i a
--- Math/MatrixOps.hs
+++ Math/MatrixOps.hs
@@ -3,17 +3,14 @@ module MatrixOps where
 import Data.List
 
 determinant :: (Num a, Fractional a) => [[a]] -> a
-
 determinant [[x]] = x
 determinant mat =
  sum [(-1)^i*x*(determinant (getRest i mat)) | (i, x) <- zip [0..] (head mat)]
 
 getRest :: (Num a, Fractional a) => Int -> [[a]] -> [[a]]
-
 getRest i mat = removeCols i (tail mat)
 
 removeCols :: (Num a, Fractional a) => Int -> [[a]] -> [[a]]
-
 removeCols _ [] = []
 removeCols i (r:rs) = (left ++ (tail right)) : removeCols i rs
  where (left, right) = splitAt i r

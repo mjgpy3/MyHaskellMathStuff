4d4d7036df0e59b0b18362f6f3b7445f2e0098bd
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-25 16:56:48 -0400

    May have made an nth matrix system solver... Will test after dinner :)

--- Math/EquationSystems.hs
+++ Math/EquationSystems.hs
@@ -1,6 +1,25 @@
 module EquationSystems where
 
-solveTwoEquations :: (Num a, Fractional a) => (a, a, a) -> (a, a, a) -> (a, a)
+import MatrixOps
 
-solveTwoEquations (a,b,c) (d,e,f) = ((c*e-b*f)/det, (a*f-c*d)/det)
- where det = a*e - b*d
+solveTwoEquations :: (Num a, Fractional a) => [[a]] -> [a] -> [a]
+
+-- Delete me after fixing the test
+solveTwoEquations a b = solveEquations a b 
+
+solveEquations :: (Num a, Fractional a) => [[a]] -> [a] -> [a]
+
+-- Solved equations of the form AX = B
+--  where the A is the coefficient matrix,
+--        X is [x, y, z, ...]
+--        B is the constants matrix
+solveEquations a b = [ determinant (replaceCols i a b) / det | i <- [0..(length a)-1] ]
+ where det = determinant a
+
+
+replaceCols :: (Num a, Fractional a) => Int -> [[a]] -> [a] -> [[a]]
+
+replaceCols _ [] [] = []
+replaceCols i (a:as) (b:bs) = (left ++ [b] ++  (tail right)) : replaceCols i as bs
+ where (left, right) = splitAt i a
+
--- Math/Test_EquationSystems.hs
+++ Math/Test_EquationSystems.hs
@@ -4,7 +4,7 @@ import EquationSystems
 import Test.HUnit
 
 testSolveTwoEquationsWorksForASimpleSystem =
-  TestCase (assertEqual "Simple system of two" (1, 2) (solveTwoEquations (2, 2, 6) (4, 0, 4)))
+  TestCase (assertEqual "Simple system of two" [1, 2] (solveTwoEquations [[2, 2], [4, 0]] [6, 4]))
 
 tests = TestList [testSolveTwoEquationsWorksForASimpleSystem]
 

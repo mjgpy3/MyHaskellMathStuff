05a1cd7595fb6a044df4bacd57dc8c98c36d2559
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-27 12:13:11 -0400

    Started adding a function to return a list of fibs

--- Fibs.hs
+++ Fibs.hs
@@ -5,3 +5,10 @@ nthFib :: (Integral a) => a -> a
 nthFib 1 = 1
 nthFib 2 = 1
 nthFib n = nthFib (n-1) + nthFib (n-2) 
+
+nFibs :: Integer -> [Integer]
+
+nFibs 0 =  []
+nFibs 1 = [1]
+nFibs 2 = [1,1]
+--nFibs n = []

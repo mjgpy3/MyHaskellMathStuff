2f89b43ace599f9af723d360e8b46f824d2f7e19
Michael Gilliland <michael@michael-writebox.(none)>
2013-04-26 21:02:45 -0400

    Added an obvious case for a 1x1

--- Math/Test_EquationSystems.hs
+++ Math/Test_EquationSystems.hs
@@ -8,6 +8,9 @@ allAreWithinDelta delta (v:vs) (e:es)
   | not (abs (e - v) < delta) = False
   | otherwise = allAreWithinDelta delta vs es
 
+testSolvingASystemOfOne =
+  TestCase (assertEqual "Simple system of one" [2] (solveEquations [[5]] [10]))
+
 testSolvingASystemOfTwo =
   TestCase (assertEqual "Simple system of two" [1, 2] (solveEquations [[2, 2], [4, 0]] [6, 4])) 
 
@@ -17,7 +20,8 @@ testSolvingASystemOfThree =
 testSolvingASystemOfFour =
   TestCase (assertEqual "Simple system of four" True (allAreWithinDelta 5e-12 [-0.521, 3.141593, 78.2,-0.27] (solveEquations [[0.72, -0.992, 7, 3.58], [911, 53.12, -81, 12.73], [0, 12.7, 0.001, -62], [0, -90.12, 0.00231, 0]] [542.941819744, -6645.38667984, 56.7164311, -282.93971916000004] )))
 
-tests = TestList [testSolvingASystemOfTwo,
+tests = TestList [testSolvingASystemOfOne,
+                  testSolvingASystemOfTwo,
                   testSolvingASystemOfThree,
                   testSolvingASystemOfFour]
 

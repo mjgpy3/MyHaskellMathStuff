986e4c05871a88b1df51a2668acb2c58290b76a4
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-05 21:24:04 -0400

    Tested whether polymorphic types can co-inhabit a list

--- Tracker/Test_Tracker.hs
+++ Tracker/Test_Tracker.hs
@@ -7,13 +7,18 @@ pikaCard = NumberedCollectable "Pikachu" 25
 geodudeCard = NumberedCollectable "Geodude" 74
 jigglyPuffCard = NumberedCollectable "Jigglypuff" 39
 charizardCard = NumberedCollectable "Charizard" 6
-guinessCap = Collectable "Guinness"
+guinnessCap = Collectable "Guinness"
 
 cardBox = Location "Deck Box" [pikaCard, geodudeCard, jigglyPuffCard, charizardCard]
+boxOfJunk = Location "Box of junk" [guinnessCap, pikaCard]
 
 testItemNumbersAffirmThatTheLocationWasConstructedCorrectly =
   TestCase (assertEqual "Testing to see if the location works" [25, 74, 39, 6] [collectableNumber card | card <- (items cardBox)])
 
-tests = TestList [testItemNumbersAffirmThatTheLocationWasConstructedCorrectly]
+testPolymorphicItemsCanBothShareAList =
+  TestCase (assertEqual "Testing whether polymorphic data can inhabit a list" ["Guinness", "Pikachu"] [collectableName thing | thing <- (items boxOfJunk)])
+
+tests = TestList [testItemNumbersAffirmThatTheLocationWasConstructedCorrectly,
+                  testPolymorphicItemsCanBothShareAList]
 
 main = runTestTT tests
